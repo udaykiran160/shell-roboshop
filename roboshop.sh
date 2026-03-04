a906372254ca3297a548b2bf934c2e65c5c7e10e
udaykiran160 <udaykirannaidulingutla@gmail.com>
2026-03-04 20:26:13 +0530

    session-6

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME="dawsuday.fun "
 
 for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID--instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output tex)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID--instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
     # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
@@ -18,7 +18,7 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME" # dawsuday.fun
     fi
-    
+
     echo "$instance: $IP"
 
     aws route53 change-resource-record-sets \
